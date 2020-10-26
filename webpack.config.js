500ec52c131331e88d7b3d8bedeeca701f67d721
chenxsan <chenxsan@gmail.com>
2020-10-26 21:12:41 +0800

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,9 @@
-const path = require('path')
-const toml = require('toml'); 
-const yaml = require('yamljs');
-const json5 = require('json5');
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
   },
 }
