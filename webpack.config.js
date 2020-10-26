69f69db09f12222be92fe5bdbca5c4a6272706a4
chenxsan <chenxsan@gmail.com>
2020-10-26 20:56:16 +0800

    load xml and csv

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,16 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader'],
+      },
     ],
   },
 }
